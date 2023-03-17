d4c97ba68a18ae5554ef6afb94c38c60729d38cc
hasnat123 <hasnat_45@hotmail.co.uk>
2023-03-17 00:06:58 +0000

    .

--- server/index.js
+++ server/index.js
@@ -48,6 +48,13 @@ app.use((err, req, res, next) =>
 
 })
 
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/dist'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html')));
+  }
+
 const startServer = async() =>
 {
     try
